3401b363deeed5f28eb8440e05afa10b4ea06b40
Lucas Garbe <hi@lucasgarbe.de>
2023-02-04 23:15:51 +0100

    tinted-theming/base16 shell integration

--- lua/user/colors.lua
+++ lua/user/colors.lua
@@ -1,3 +1,12 @@
-vim.cmd [[
-  source ~/.vimrc_background
-]]
+-- vim.cmd [[
+--   source ~/.vimrc_background
+-- ]]
+
+local cmd = vim.cmd
+local g = vim.g
+
+local current_theme_name = os.getenv('BASE16_THEME')
+if current_theme_name and g.colors_name ~= 'base16-'..current_theme_name then
+  cmd('let base16colorspace=256')
+  cmd('colorscheme base16-'..current_theme_name)
+end
